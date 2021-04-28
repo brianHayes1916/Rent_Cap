449bf0e0dca17d075d4540df9065526b17727633
Brian Hayes <briancampbellhayes@icloud.com>
2021-04-28 13:24:00 -0500

    Adds the listing and User model. Both need expantion but are functional for now.

---
+++ Renter Capstone/Models/Listing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Renter_Capstone.Models
+{
+    public class Listing
+    {
+        [Key]
+        public int ListingId { get; set; }
+        public int Prioirty { get; set; }
+        public string Images { get; set; }
+        public int Cost { get; set; }
+        public string Description { get; set; }
+        public int SquareFeet { get; set; }
+
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+        
+    }
+}
---
+++ Renter Capstone/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Renter_Capstone.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public bool Renter { get; set; }
+        public bool Leasing { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+
+        [ForeignKey("Listing")]
+        public int ListingId { get; set; }
+        public Listing Listing { get; set; }
+    }
+}
